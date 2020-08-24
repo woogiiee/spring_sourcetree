186c07f3afd973d77945f81da7ac0068b8faeda9
woogiiee <65659198+woogiiee@users.noreply.github.com>
2020-08-24 10:56:07 +0900

    develop version1

--- src/main/java/edu/bit/ex/HomeController2.java
+++ src/main/java/edu/bit/ex/HomeController2.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 
-//version-1
-//version-2
-//version-3
+//version1
 @Controller
 public class HomeController2 {
 	
